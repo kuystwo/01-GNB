5f292d052b0f855217c4a2e59915491ed9cfef7b
jisungyeon <oracle1120@naver.com>
2023-11-29 12:10:12 +0900

    버전3 완성

---
+++ 03/js/script.js
@@ -0,0 +1,26 @@
+$(function () {
+  const $window = $(window);
+  const $header = $('#header');
+  const $menu = $('.gnb > li');
+  const $submenu = $('.submenu');
+  const duration = 300;
+
+  $menu.on('mouseenter', function () {
+    $(this).addClass('on');
+    $header.addClass('active');
+    $submenu.stop().fadeIn(duration);
+  });
+
+  $menu.on('mouseleave', function () {
+    $menu.removeClass('on');
+    $header.removeClass('active');
+    $submenu.stop().fadeOut(duration);
+  });
+
+  // 마우스 휠을 조작했을 때 : wheel
+  $window.on('wheel', function (e) {
+    e.originalEvent.wheelDelta > 0
+      ? $header.Class('hide') /* true */
+      : $header.removeClass('hide'); /* false */
+  });
+});
